d86f5971c0df342d574ec284f529c70a9fed4da8
Kadeh <kikpe1@students.towson.edu>
2022-12-11 15:23:11 -0500

    Added events page

--- app/src/main/java/com/example/groupproject/EventsActivity.kt
+++ app/src/main/java/com/example/groupproject/EventsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.groupproject
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.groupproject.ui.theme.GroupProjectTheme
+import android.webkit.WebViewClient
+import androidx.compose.material.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.viewinterop.AndroidView
+
+import androidx.compose.material.Scaffold
 
 class EventsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +30,7 @@ class EventsActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    MainContent()
 
 
 
@@ -32,14 +41,40 @@ class EventsActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainContent() {
+    Scaffold(
+        topBar = { TopAppBar(
+            title = { Text("Involed at TU", color = Color.White) },
+            backgroundColor = Color(4294885393)) },
+        content = { MyContent() }
+    )
+}
+@Composable
+fun MyContent(){
+    val mUrl = "https://www.towson.edu/calendars/"
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(mUrl)
+        }
+    }, update = {
+        it.loadUrl(mUrl)
+    })
+}
+
+@Composable
 fun Greeting8(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Welcome $name!!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview8() {
     GroupProjectTheme {
-        Greeting8("Android")
+        Greeting8("User")
     }
 }
